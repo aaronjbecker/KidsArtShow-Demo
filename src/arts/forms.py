2b7a497a9f24f80a8308dcc5d72803f5a59a355c
Zhang <Wenliang.Zhang@PerkinElmer.com>
2018-09-26 15:07:24 -0400

    create forms and add create and update view

--- src/arts/forms.py
+++ src/arts/forms.py
@@ -1,5 +1,60 @@
 from django import forms
+from django.forms import ModelForm
 
-class ArtAddFrom(forms.Form):
-    title = forms.CharField()
-    description = forms.CharField()
+from .models import Art
+
+CATEGORY_CHOICES = (
+    ('drawings',"Drawings"),
+    ('paintings',"Paintings"),
+    ('ceramics','Ceramics'),
+    ('collage' ,'Collage'),
+    ('sculpture',"Sculpture")
+)
+class ArtAddForm(forms.Form):
+    title = forms.CharField(label='Art Title',widget=forms.TextInput(
+        attrs={
+            'class':'custom-class',
+            'placeholder':'Title'
+    }))
+    description = forms.CharField(widget=forms.Textarea(
+        attrs={
+            'class':'my-custom-class',
+            'placeholder':'Description'
+        }
+    ))
+    category = forms.ChoiceField(choices = CATEGORY_CHOICES)
+
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if len(title) <= 3:
+            raise forms.ValidationError('Tile must be at least 3 characters or longer')
+        else:
+            return title
+
+
+class ArtModelForm(forms.ModelForm):
+    # adds new fields in regular django form, if there are same fields from model, it over-rides it
+    category = forms.ChoiceField(choices = CATEGORY_CHOICES)
+    # add fields coming from model
+    class Meta:
+        model = Art
+        fields = [
+            'title',
+            'description',
+            'category'
+        ]
+        widgets = {
+            forms.Textarea(
+                attrs={
+                    'class': 'my-custom-class',
+                    'placeholder': 'Description'
+                }
+            )
+        }
+
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if len(title) <= 3:
+            raise forms.ValidationError('Tile must be at least 3 characters or longer')
+        else:
+            return title
--- src/arts/views.py
+++ src/arts/views.py
@@ -1,16 +1,48 @@
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 
-from .forms import ArtAddFrom
+from .forms import ArtAddForm, ArtModelForm
 from .models import Art
 # Create your views here.
 
+def update_view(request,object_id):
+    art = get_object_or_404(Art,id = object_id)
+    form = ArtModelForm(request.POST or None, instance=art)
+    if form.is_valid():
+        instance = form.save(commit = False)
+        instance.save()
+    template = 'form.html'
+    context = {
+        'art': art,
+        'form':form,
+        'submit_btn':'Update Art'
+    }
+    return render(request, template, context)
+
 def create_view(request):
     print(request.POST)
-    form = ArtAddFrom()
-    template = 'create_view.html'
+    form = ArtModelForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit = False)
+        instance.save()
+    # form = ArtAddForm(request.POST or None)
+    # if request.method == 'POST':
+    #     print(request.POST.get('title'))
+    # if form.is_valid():
+    #     data = form.cleaned_data
+    #     title = data.get('title')
+    #     description = data.get('description')
+    #     category = data.get('category')
+    #     new_obj = Art()
+    #     new_obj.title = title
+    #     new_obj.description = description
+    #     new_obj.category = category
+
+        # print(request.POST.get('title'))
+    template = 'form.html'
     context = {
         'form': form,
+        'submit_btn': 'Create Art'
     }
     return render(request, template, context)
 
@@ -35,6 +67,9 @@ def detail_view(request, object_id = None):
 
 def list_view(request):
     print(request)
+    queryset = Art.objects.all()
     template = 'list_view.html'
-    context = {}
+    context = {
+        'queryset' : queryset
+    }
     return render(request, template, context)
