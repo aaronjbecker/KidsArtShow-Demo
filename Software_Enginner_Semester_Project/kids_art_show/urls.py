c98aa231e061d28af9157faa5810660e23f13297
Aaron Becker <aaronjamesbecker@gmail.com>
2018-10-28 20:09:10 -0400

    consolidating packages/simplifying app structure, step 1

--- Software_Enginner_Semester_Project/kids_art_show/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name = 'kids-art-show-home'),
-    path('about', views.about, name = 'kids-art-show-about')
-]
