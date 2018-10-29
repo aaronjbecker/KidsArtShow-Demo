27963990f3861b8a1fc549aeb0a955d2d67f7182
Aaron Becker <aaronjamesbecker@gmail.com>
2018-10-29 19:47:17 -0400

    remove user app

--- src/kids_art_show/settings.py
+++ src/kids_art_show/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'kids_art_show.apps.KidsArtShowConfig',
-    'users.apps.UsersConfig',
     'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
