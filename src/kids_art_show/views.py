b98047f26a68c19c18f1b8d122289ffa6698f19d
Aaron Becker <aaronjamesbecker@gmail.com>
2018-10-29 19:45:03 -0400

    include context with views, commented register in case it's needed

--- src/kids_art_show/views.py
+++ src/kids_art_show/views.py
@@ -2,16 +2,42 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import KidsArtShowUserCreationForm
+from .models import Post
 
 
 def home(request):
-    return render(request, 'home.html')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'home.html', context)
+
 
 def about(request):
-    return render(request, 'about.html')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'about.html', context)
+
 
 class SignUp(generic.CreateView):
     form_class = KidsArtShowUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
 
+
+# TODO: how to merge the lines below with the signup view above?
+# from django.shortcuts import render, redirect
+# from django.contrib import messages
+# from .forms import UesrRegisterForm
+#
+# def register(request):
+#     if request.method == 'POST':
+#         form = UesrRegisterForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data.get('username')
+#             messages.success(request, f'Account created for {username}!')
+#             return redirect('kids-art-show-home')
+#     else:
+#         form = UesrRegisterForm()
+#     return render(request, 'users/register.html', {'form' : form})
