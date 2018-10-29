7e42f969fbc1abaded6737c9a11501e76342cac8
Aaron Becker <aaronjamesbecker@gmail.com>
2018-10-29 19:45:41 -0400

    consolidate admin screens/models

--- src/kids_art_show/admin.py
+++ src/kids_art_show/admin.py
@@ -1,18 +1,27 @@
 """
 AJB 10/28/18: adapted from https://wsvincent.com/django-custom-user-model-tutorial/
-"""
 # Register your admin models here.
+"""
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
-
 from .forms import KidsArtShowUserCreationForm, KidsArtShowUserChangeForm
-from .models import KidsArtShowUser
+from .models import KidsArtShowUser, Post
+
 
 class KidsArtShowAdmin(UserAdmin):
+    """
+    This class determines the fields that appear in the user administration view
+    """
     add_form = KidsArtShowUserCreationForm
     form = KidsArtShowUserChangeForm
     model = KidsArtShowUser
+    # TODO: better list of fields to display for user admin?
     list_display = ['email', 'username', 'birth_date', 'bio']
 
+# TODO: create post administration view?
+
+# register admin objects and/or model classes
 admin.site.register(KidsArtShowUser, KidsArtShowAdmin)
+admin.site.register(Post)
+
