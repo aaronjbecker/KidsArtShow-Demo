e33987b931c245474eaf67f2024056a90997f77a
Aaron Becker <aaronjamesbecker@gmail.com>
2018-10-28 23:21:55 -0400

    customized admin views etc. based on tutorials (not tested)

--- src/kids_art_show/admin.py
+++ src/kids_art_show/admin.py
@@ -1,3 +1,18 @@
+"""
+AJB 10/28/18: adapted from https://wsvincent.com/django-custom-user-model-tutorial/
+"""
+# Register your admin models here.
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import KidsArtShowUserCreationForm, KidsArtShowUserChangeForm
+from .models import KidsArtShowUser
+
+class KidsArtShowAdmin(UserAdmin):
+    add_form = KidsArtShowUserCreationForm
+    form = KidsArtShowUserChangeForm
+    model = KidsArtShowUser
+    list_display = ['email', 'username', 'birth_date', 'bio']
+
+admin.site.register(KidsArtShowUser, KidsArtShowAdmin)
