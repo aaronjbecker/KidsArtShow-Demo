5155688880aaab4a79beeaedad910e567e638715
Aaron Becker <aaronjamesbecker@gmail.com>
2018-11-21 15:15:43 -0500

    changes for compatibility with newer django

--- src/remember_me/views.py
+++ src/remember_me/views.py
@@ -4,10 +4,13 @@ from django.conf import settings
 from django.contrib.auth import REDIRECT_FIELD_NAME
 # Avoid shadowing the login() view below.
 from django.contrib.auth import login as auth_login
-from django.contrib.sites.models import get_current_site, Site
+from django.contrib.sites.shortcuts import get_current_site
+# from django.contrib.sites.models import Site
 from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+# cf. https://stackoverflow.com/a/38739423
+from django.shortcuts import render
+# from django.shortcuts import render_to_response
+# from django.template import RequestContext
 from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_protect
 from remember_me.forms import AuthenticationRememberMeForm
@@ -33,7 +36,7 @@ def remember_me_login(request, template_name='registration/login.html',
     
     """
     
-    redirect_to = request.REQUEST.get(redirect_field_name, '')
+    redirect_to = request.POST.get(redirect_field_name, '')
     
     if request.method == "POST":
         form = authentication_form(data=request.POST)
@@ -67,9 +70,10 @@ def remember_me_login(request, template_name='registration/login.html',
     
     current_site = get_current_site(request)
     
-    return render_to_response(template_name, {
-        'form': form,
-        redirect_field_name: redirect_to,
-        'site': current_site,
-        'site_name': current_site.name,
-    }, context_instance=RequestContext(request))
+    return render(request, template_name,
+                  {
+                      'form': form,
+                      redirect_field_name: redirect_to,
+                      'site': current_site,
+                      'site_name': current_site.name,
+                  })
