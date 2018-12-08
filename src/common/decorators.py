d8616d77249fdfcfc71e76d3d01b520b2bd243a6
Aaron Becker <aaronjamesbecker@gmail.com>
2018-12-08 11:00:56 -0500

    adds ajax required decorator

---
+++ src/common/decorators.py
@@ -0,0 +1,16 @@
+"""
+AJB 12/8/18
+Copied from https://github.com/PacktPublishing/Django-2-by-Example
+Above repo is released under MIT license, which allows re-use.
+"""
+from django.http import HttpResponseBadRequest
+
+
+def ajax_required(f):
+    def wrap(request, *args, **kwargs):
+        if not request.is_ajax():
+            return HttpResponseBadRequest()
+        return f(request, *args, **kwargs)
+    wrap.__doc__=f.__doc__
+    wrap.__name__=f.__name__
+    return wrap
