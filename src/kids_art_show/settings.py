9e8764b5fbb8fb4097c452350087f5996a119692
Aaron Becker <aaronjamesbecker@gmail.com>
2018-12-09 18:46:11 -0500

    better art list and detail views

--- src/kids_art_show/settings.py
+++ src/kids_art_show/settings.py
@@ -21,6 +21,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
